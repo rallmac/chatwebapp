c45ac1c71570bfdfd504fbb8582293c5cd3f8ea8
rallmac2 <root@tobijahdevice>
2024-08-25 01:27:47 +0100

    tatabase structure

---
+++ database/contactsandfriends_table.sql
@@ -0,0 +1,12 @@
+--This table contains contacts added to a profile
+
+CREATE TABLE contacts_table (
+	user_id INT NOT NULL,
+	contact_user_id INT NOT NULL,
+	status ENUM('pending', 'accepted') NOT NULL,
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (user_id, contact_user_id),
+	FOREIGN KEY (user_id) REFERENCES users_table(user_id),
+	FOREIGN KEY (contact_user_id) REFERENCES users_table(user_id)
+);
